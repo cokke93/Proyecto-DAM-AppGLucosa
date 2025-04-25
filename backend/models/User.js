4740dd8ae780496e9899de56a5426d1c6403d453
cokke93 <cokkecs@gmail.com>
2025-04-25 16:59:17 +0200

    Implementada la funcionalidad de hashear el password del usuario al registrarse con brypt.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt   = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -22,7 +23,24 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Middleware to hash password before saving
+//If password is modified, hash it
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt    = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Compare login
+userSchema.methods.comparePassword = function(candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+//Remove password from JSON response
 userSchema.methods.toJSON = function() {
   const obj = this.toObject();
   delete obj.password;
